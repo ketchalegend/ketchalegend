c9e43978a9db0de042cd7d72a850a4e2847ea0a8
ketchalegend <kbepa02@gmail.com>
2022-07-06 22:54:31 +0200

    fixed errors

--- pages/works.js
+++ pages/works.js
@@ -1,6 +1,6 @@
 
 
-import { Container, Link, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
