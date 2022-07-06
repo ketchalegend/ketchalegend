11e2b0a2360d5d7393105329c05d6af48c557c7f
ketchalegend <kbepa02@gmail.com>
2022-07-06 20:39:59 +0200

    code

---
+++ pages/index.js
@@ -0,0 +1,169 @@
+import NextLink from 'next/link'
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Paragraph from '../components/paragraph'
+import { BioSection, BioYear } from '../components/bio'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { GridItem } from '../components/grid-item'
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5'
+import Image from 'next/image'
+
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
+})
+
+const Home = () => (
+  <Layout>
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I&apos;m an indie web developer based in Germany!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Emmanuel Ketcha
+          </Heading>
+          <p>Software Engineer</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/emmanuel.jpeg"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
+        </Box>
+      </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+        Emmanuel is a full-stack developer based in Cologne with a
+          passion for building digital services/stuff he wants. He has a knack
+          for all things launching products, from planning and designing all the
+          way to solving real-life problems with code. When not online, he loves
+          travelling around the world and meeting new people. Currently, he is working on different projects like {' '}
+          <NextLink href="https://linkfro.de" passHref scroll={false}>
+            <Link>Linkfro</Link>
+          </NextLink> a link-in-bio platform, <NextLink href="https://alfrica.com" passHref scroll={false}>
+            <Link>Alfrica</Link>
+          </NextLink> a black owned search engine. 
+        </Paragraph>
+        
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2014 - 2018</BioYear>
+          Bachelor Electrical Engineering (RWTH Aachen University)
+          </BioSection>
+        <BioSection>
+          <BioYear>2018 - 2021</BioYear>
+          Worked at m3connect as Software Engineer
+        </BioSection>
+        <BioSection>
+          <BioYear>2021 to present</BioYear>
+          Software Engineer and freelancer
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Art, Music, Photography, Travelling and programming
+          </Paragraph>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/ketchalegend" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoGithub />}
+              >
+                @ketchalegend
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/ketchabepa" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @ketchabepa
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://instagram.com/ketchalegend" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoInstagram />}
+              >
+                @ketchalegend
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+
+
+        
+      </Section>
+    </Container>
+  </Layout>
+)
+
+export default Home
+export { getServerSideProps } from '../components/chakra'
---
+++ pages/works.js
@@ -0,0 +1,61 @@
+
+
+import { Container, Link, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+
+import thumbLinkfro from '../public/images/works/linkfro.png'
+import thumbAfroPlus from '../public/images/works/afroplus.png'
+import thumbSearchEngine from '../public/images/works/searchengine.png'
+
+
+
+const Works = () => (
+  <Layout title="Works">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Works
+      </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem id="" title="Linkfro" thumbnail={thumbLinkfro}>
+          The most powerful website builder for entrepreneurs, creators, artists, and small businesses with over 4000+ users. 
+          </WorkGridItem>
+        </Section>
+
+
+        <Section>
+          <WorkGridItem
+            id=""
+            title="AfroPlus"
+            thumbnail={thumbAfroPlus}
+          >
+            Black owned business search directory
+          </WorkGridItem>
+        </Section>
+
+        <Section delay={0.1}>
+          <WorkGridItem
+            id=""
+            title="Alfrica"
+            thumbnail={thumbSearchEngine}
+          >
+            The ultimate black owned search engine with african related results for images, videos, news and more. 
+          </WorkGridItem>
+        </Section>
+
+      </SimpleGrid>
+
+      
+
+        
+
+
+    </Container>
+  </Layout>
+)
+
+export default Works
+export { getServerSideProps } from '../components/chakra'
